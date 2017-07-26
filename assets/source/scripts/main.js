e10ca3a44d0cbffb83c412fe80dab6368992da7f
julia.volkova090@gmail.com <LF4bjJfMk2w5Pl8h>
2017-07-26 16:55:26 +0300

    Validation form, license's counter

--- assets/source/scripts/main.js
+++ assets/source/scripts/main.js
@@ -4,18 +4,23 @@ var modal_wrap = document.querySelector('.js-modal-wrap');
 var background = document.querySelector('.js-background');
 var open_coupone_input = document.querySelector('.js-open-coupone-input');
 var open_coupone_btn = document.querySelector('.js-open-coupone-btn');
-var coupone_number = document.querySelector('.js-coupone-number');
 var close_modal = document.querySelector('.js-close-modal');
 var open_new_card = document.querySelector('.js-open-form');
 var close_new_card = document.querySelector('.js-close-form');
 var new_card = document.querySelector('.js-form');
-var payment = document.querySelector('#payment');
 var initial_state = {
     license: 3,
     cost: 5.99
 };
-var time = document.querySelectorAll('.js-time');
+var sale = {
+    1: 0,
+    6: 3.33,
+    9: 5,
+    12: 8.33,
+    24: 13.33
+};
 var discount = document.querySelectorAll('.js-sale');
+var discount_total = document.querySelectorAll('.js-total-sale');
 var total = document.querySelectorAll('.js-total-price');
 var license = document.querySelector('.js-license-count');
 var lisence_count;
@@ -23,6 +28,10 @@ var license_reduction = document.querySelector('.js-reduction');
 var license_raising = document.querySelector('.js-raising');
 var time_price = document.querySelectorAll('.js-time-price');
 var radio = document.querySelectorAll('.js-time-change');
+
+var input_number = document.querySelectorAll('.js-number');
+var input_number_new_card = document.querySelector('.js-number-new-card');
+var license_count_value = document.querySelectorAll('.js-license-count-value');
 
 license.innerHTML = initial_state.license;
 
@@ -59,12 +68,8 @@ background.addEventListener('click', function () {
     document.body.style.overflow = 'auto';
 });
 
-console.log(modal.classList.contains('open'));
 
-
-
-
-
+// License's Counter
 license_reduction.addEventListener('click', function () {
     changeCount(-1);
 });
@@ -88,14 +93,93 @@ function calculate() {
     const time = document.querySelector('.js-time-change:checked').dataset.time;
     lisence_count = initial_state.license;
 
+    license_count_value.forEach(function (el) {
+        el.innerHTML = lisence_count;
+    });
+
     time_price.forEach(function (el, i) {
-        el.innerHTML = '$' + (el.dataset.time * lisence_count * initial_state.cost).toFixed(2);
+        if (el.dataset.time == 1) {
+            el.innerHTML = '$' + '0';
+        } else {
+            el.innerHTML = '$' + (el.dataset.time * lisence_count * initial_state.cost).toFixed(2);
+        }
     });
 
     total.forEach(function (element, i) {
-        var result = (time * lisence_count * initial_state.cost).toFixed(2);
-        element.innerHTML = '$' + result;
+        var result;
+        if (time == 1) {
+            result = +'0';
+        }
+        else {
+            result = (time * lisence_count * initial_state.cost).toFixed(2);
+            result = result;
+        }
+
+        discount.forEach(function (el) {
+            var current_sale;
+            var months = el.dataset.time;
+            if(lisence_count >= 3) {
+                current_sale = Math.ceil(lisence_count * sale[months]);
+            } else {
+                // current_sale = +'0';
+            }
+            el.innerHTML = '$' + current_sale + ' off';
+            result = (result - current_sale).toFixed(2);
+            element.innerHTML = '$' + result;
+        });
+    });
+
+    discount_total.forEach(function (el) {
+        var result;
+        var months =  document.querySelector('.js-time-change:checked').dataset.time;
+        if(lisence_count >= 3) {
+            result = Math.ceil(lisence_count * sale[months]);
+        } else {
+            result = +'0';
+        }
+        el.innerHTML = '$' + result + ' off';
     });
 }
 
 calculate();
+
+
+// Validation form
+input_number.forEach(function (el) {
+    el.onkeypress = function(e) {
+        e = e || event;
+
+        if (e.ctrlKey || e.altKey || e.metaKey) return;
+
+        var chr = getChar(e);
+
+        if (chr == null) return;
+
+        if (chr < '0' || chr > '9') {
+            return false;
+        }
+    };
+
+    function getChar(event) {
+        if (event.which == null) {
+            if (event.keyCode < 32) return null;
+            return String.fromCharCode(event.keyCode)
+        }
+
+        if (event.which != 0 && event.charCode != 0) {
+            if (event.which < 32) return null;
+            return String.fromCharCode(event.which)
+        }
+
+        return null;
+    }
+});
+
+// Number credit-card
+input_number_new_card.addEventListener('keypress', formatCardCode, false);
+
+function formatCardCode() {
+    var cardCode = this.value.replace(/[^\d]/g, '').substring(0,16);
+    cardCode = cardCode != '' ? cardCode.match(/.{1,4}/g).join(' ') : '';
+    this.value = cardCode;
+}
