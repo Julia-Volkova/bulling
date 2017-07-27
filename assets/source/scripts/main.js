ac1d8ccb9550e2c44403d219c2eab22be90f0046
julia.volkova090@gmail.com <LF4bjJfMk2w5Pl8h>
2017-07-27 12:18:23 +0300

    Refactoring

--- assets/source/scripts/main.js
+++ assets/source/scripts/main.js
@@ -14,10 +14,10 @@ var initial_state = {
 };
 var sale = {
     1: 0,
-    6: 3.33,
-    9: 5,
-    12: 8.33,
-    24: 13.33
+    6: 10,
+    9: 15,
+    12: 25,
+    24: 40
 };
 var discount = document.querySelectorAll('.js-sale');
 var discount_total = document.querySelectorAll('.js-total-sale');
@@ -32,6 +32,7 @@ var radio = document.querySelectorAll('.js-time-change');
 var input_number = document.querySelectorAll('.js-number');
 var input_number_new_card = document.querySelector('.js-number-new-card');
 var license_count_value = document.querySelectorAll('.js-license-count-value');
+var input_month = document.querySelector('.js-input-month');
 
 license.innerHTML = initial_state.license;
 
@@ -57,11 +58,11 @@ open_new_card.addEventListener('click', function () {
     new_card.classList.add('show');
     modal_wrap.style.marginTop = '-295px';
 });
-
-close_new_card.addEventListener('click', function () {
-    new_card.classList.remove('show');
-    modal_wrap.style.marginTop = '-172px';
-});
+//
+// close_new_card.addEventListener('click', function () {
+//     new_card.classList.remove('show');
+//     modal_wrap.style.marginTop = '-172px';
+// });
 
 background.addEventListener('click', function () {
     modal.classList.remove('open');
@@ -71,7 +72,9 @@ background.addEventListener('click', function () {
 
 // License's Counter
 license_reduction.addEventListener('click', function () {
-    changeCount(-1);
+    if (initial_state.license > 3) {
+        changeCount(-1);
+    }
 });
 
 license_raising.addEventListener('click', function () {
@@ -92,6 +95,8 @@ function changeCount(val) {
 function calculate() {
     const time = document.querySelector('.js-time-change:checked').dataset.time;
     lisence_count = initial_state.license;
+    var current_sale = [];
+    console.log(current_sale);
 
     license_count_value.forEach(function (el) {
         el.innerHTML = lisence_count;
@@ -105,6 +110,23 @@ function calculate() {
         }
     });
 
+    discount.forEach(function (el) {
+        var months = el.dataset.time;
+        current_sale = Math.ceil(lisence_count * sale[months]);
+        el.innerHTML = '$' + current_sale + ' off';
+    });
+
+    discount_total.forEach(function (el) {
+        var result;
+
+        if (lisence_count >= 3) {
+            result = Math.ceil(lisence_count * sale[time]);
+        } else {
+            result = +'0';
+        }
+        el.innerHTML = '$' + result + ' off';
+    });
+
     total.forEach(function (element, i) {
         var result;
         if (time == 1) {
@@ -112,41 +134,19 @@ function calculate() {
         }
         else {
             result = (time * lisence_count * initial_state.cost).toFixed(2);
-            result = result;
+            result -= current_sale[i];
         }
-
-        discount.forEach(function (el) {
-            var current_sale;
-            var months = el.dataset.time;
-            if(lisence_count >= 3) {
-                current_sale = Math.ceil(lisence_count * sale[months]);
-            } else {
-                // current_sale = +'0';
-            }
-            el.innerHTML = '$' + current_sale + ' off';
-            result = (result - current_sale).toFixed(2);
-            element.innerHTML = '$' + result;
-        });
-    });
-
-    discount_total.forEach(function (el) {
-        var result;
-        var months =  document.querySelector('.js-time-change:checked').dataset.time;
-        if(lisence_count >= 3) {
-            result = Math.ceil(lisence_count * sale[months]);
-        } else {
-            result = +'0';
-        }
-        el.innerHTML = '$' + result + ' off';
+        element.innerHTML = '$' + result;
     });
 }
 
 calculate();
+// End License's Counter
 
 
 // Validation form
 input_number.forEach(function (el) {
-    el.onkeypress = function(e) {
+    el.onkeypress = function (e) {
         e = e || event;
 
         if (e.ctrlKey || e.altKey || e.metaKey) return;
@@ -159,27 +159,58 @@ input_number.forEach(function (el) {
             return false;
         }
     };
-
-    function getChar(event) {
-        if (event.which == null) {
-            if (event.keyCode < 32) return null;
-            return String.fromCharCode(event.keyCode)
-        }
-
-        if (event.which != 0 && event.charCode != 0) {
-            if (event.which < 32) return null;
-            return String.fromCharCode(event.which)
-        }
-
-        return null;
-    }
 });
+
+input_month.onkeypress = function (e) {
+    e = e || event;
+
+    if (e.ctrlKey || e.altKey || e.metaKey) return;
+
+    var chr = getCharBelow13(e);
+
+    if (chr == null) return;
+
+    var result = chr.match([1 - 9]);
+
+    console.log(result);
+    if (result) {
+        addZero(chr, 2);
+    }
+
+    if (chr < '0' || chr > '9') {
+        return false;
+    }
+};
+
 
 // Number credit-card
 input_number_new_card.addEventListener('keypress', formatCardCode, false);
 
 function formatCardCode() {
-    var cardCode = this.value.replace(/[^\d]/g, '').substring(0,16);
+    var cardCode = this.value.replace(/[^\d]/g, '').substring(0, 16);
     cardCode = cardCode != '' ? cardCode.match(/.{1,4}/g).join(' ') : '';
     this.value = cardCode;
+}
+
+function getChar(event) {
+    if (event.which == null) {
+        if (event.keyCode < 32) return null;
+        return String.fromCharCode(event.keyCode);
+    }
+
+    if (event.which != 0 && event.charCode != 0) {
+        if (event.which < 32) return null;
+        return String.fromCharCode(event.which);
+    }
+
+    return null;
+}
+
+function addZero(n, needLength) {
+    needLength = needLength || 2;
+    n = String(n);
+    while (n.length < needLength) {
+        n = "0" + n;
+    }
+    return n
 }
