1afefb085c65da4e102b5f9ccbe79271282cb19a
julia.volkova090@gmail.com <LF4bjJfMk2w5Pl8h>
2017-07-28 13:39:15 +0300

    Change month input, v-icon, window-scroll

--- assets/source/scripts/main.js
+++ assets/source/scripts/main.js
@@ -1,4 +1,5 @@
-var btn_open_modal = document.querySelectorAll('.js-modal-open');
+//Variables
+var btn_open_modal = qsAll('.js-modal-open');
 var modal = document.querySelector('.js-modal');
 var modal_wrap = document.querySelector('.js-modal-wrap');
 var background = document.querySelector('.js-background');
@@ -19,61 +20,47 @@ var sale = {
     12: 25,
     24: 40
 };
-var discount = document.querySelectorAll('.js-sale');
-var discount_total = document.querySelectorAll('.js-total-sale');
-var total = document.querySelectorAll('.js-total-price');
+var discount = qsAll('.js-sale');
+var discount_total = qsAll('.js-total-sale');
+var total = qsAll('.js-total-price');
 var license = document.querySelector('.js-license-count');
 var lisence_count;
 var license_reduction = document.querySelector('.js-reduction');
 var license_raising = document.querySelector('.js-raising');
-var time_price = document.querySelectorAll('.js-time-price');
-var radio = document.querySelectorAll('.js-time-change');
+var time_price = qsAll('.js-time-price');
+var radio = qsAll('.js-time-change');
 
-var input_number = document.querySelectorAll('.js-number');
+var input_number = qsAll('.js-number');
 var input_number_new_card = document.querySelector('.js-number-new-card');
-var license_count_value = document.querySelectorAll('.js-license-count-value');
+var license_count_value = qsAll('.js-license-count-value');
 var input_month = document.querySelector('.js-input-month');
-
-license.innerHTML = initial_state.license;
-
-btn_open_modal.forEach(function (element) {
-    element.addEventListener('click', function (e) {
-        e.preventDefault();
-        modal.classList.add('open');
-        document.body.style.overflow = 'hidden';
-    });
-});
-
-open_coupone_input.addEventListener('click', function (e) {
-    e.preventDefault();
-    open_coupone_btn.classList.toggle('btn-open');
-});
-
-close_modal.addEventListener('click', function () {
-    modal.classList.remove('open');
-    document.body.style.overflow = 'auto';
-});
-
-background.addEventListener('click', function () {
-    modal.classList.remove('open');
-    document.body.style.overflow = 'auto';
-});
+var billing = document.querySelector('.js-billing');
 
 
-// License's Counter
-license_reduction.addEventListener('click', function () {
-    if (initial_state.license > 3) {
-        changeCount(-1);
+//Functions
+function formatCardCode() {
+    var cardCode = this.value.replace(/[^\d]/g, '').substring(0, 16);
+    cardCode = cardCode != '' ? cardCode.match(/.{1,4}/g).join(' ') : '';
+    this.value = cardCode;
+}
+
+function getChar(event) {
+    if (event.which == null) {
+        if (event.keyCode < 32) return null;
+        return String.fromCharCode(event.keyCode);
     }
-});
 
-license_raising.addEventListener('click', function () {
-    changeCount(1);
-});
+    if (event.which != 0 && event.charCode != 0) {
+        if (event.which < 32) return null;
+        return String.fromCharCode(event.which);
+    }
 
-radio.forEach(function (el) {
-    el.addEventListener('change', calculate);
-});
+    return null;
+}
+
+function qsAll(query) {
+    return [].slice.call(document.querySelectorAll(query));
+}
 
 function changeCount(val) {
     initial_state.license += val;
@@ -126,6 +113,52 @@ function calculate() {
     });
 }
 
+
+license.innerHTML = initial_state.license;
+
+btn_open_modal.forEach(function (element) {
+    element.addEventListener('click', function (e) {
+        e.preventDefault();
+        modal.classList.add('open');
+        document.body.style.overflow = 'hidden';
+        billing.classList.add('scroll');
+    });
+});
+
+open_coupone_input.addEventListener('click', function (e) {
+    e.preventDefault();
+    open_coupone_btn.classList.toggle('btn-open');
+});
+
+close_modal.addEventListener('click', function () {
+    modal.classList.remove('open');
+    document.body.style.overflow = 'auto';
+    billing.classList.remove('scroll');
+});
+
+background.addEventListener('click', function () {
+    modal.classList.remove('open');
+    document.body.style.overflow = 'auto';
+    billing.classList.remove('scroll');
+});
+
+
+// License's Counter
+license_reduction.addEventListener('click', function () {
+    if (initial_state.license > 3) {
+        changeCount(-1);
+    }
+});
+
+license_raising.addEventListener('click', function () {
+    changeCount(1);
+});
+
+radio.forEach(function (el) {
+    el.addEventListener('change', calculate);
+});
+
+
 calculate();
 // End License's Counter
 
@@ -147,56 +180,20 @@ input_number.forEach(function (el) {
     };
 });
 
-input_month.onkeypress = function (e) {
-    e = e || event;
-
-    if (e.ctrlKey || e.altKey || e.metaKey) return;
-
-    var chr = getCharBelow13(e);
-
-    if (chr == null) return;
-
-    var result = chr.match([1 - 9]);
-
-    console.log(result);
-    if (result) {
-        addZero(chr, 2);
-    }
-
-    if (chr < '0' || chr > '9') {
-        return false;
+// Month on card
+input_month.onkeyup = function (e) {
+    var value = input_month.value;
+    var result = value.match(/\b([0-9]|1[0-2])\b/g);
+    if (result == null) {
+        input_month.value = '';
     }
 };
-
 
 // Number credit-card
 input_number_new_card.addEventListener('keypress', formatCardCode, false);
 
-function formatCardCode() {
-    var cardCode = this.value.replace(/[^\d]/g, '').substring(0, 16);
-    cardCode = cardCode != '' ? cardCode.match(/.{1,4}/g).join(' ') : '';
-    this.value = cardCode;
+function changeSizeWindow() {
+    document.body.clientWidth = document.body.scroll;
 }
 
-function getChar(event) {
-    if (event.which == null) {
-        if (event.keyCode < 32) return null;
-        return String.fromCharCode(event.keyCode);
-    }
-
-    if (event.which != 0 && event.charCode != 0) {
-        if (event.which < 32) return null;
-        return String.fromCharCode(event.which);
-    }
-
-    return null;
-}
-
-function addZero(n, needLength) {
-    needLength = needLength || 2;
-    n = String(n);
-    while (n.length < needLength) {
-        n = "0" + n;
-    }
-    return n
-}
+changeSizeWindow();
